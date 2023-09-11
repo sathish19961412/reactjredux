3eccc9df74d53c35623082b551c805bf9cd9ac9d
sathish19961412 <sathishfreelanc5@gmail.com>
2023-09-11 16:17:09 +0530

    Just create Login and Profile Component Created

---
+++ src/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile=()=>{
+  return (
+    <div>
+        <h2>Profile Info</h2>
+        <h3>Name:</h3>
+        <h3>Age:</h3>
+        <h3>Email:</h3>
+    </div>
+  )
+}
+
+export default Profile
