4a9ef605101f764c81e1b388d1d01eda35405abf
sathish19961412 <sathishfreelanc5@gmail.com>
2023-09-28 15:32:24 +0530

    redux use to the configureStore and Provider in @reduxjs/toolkit/react-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+
+const store=configureStore({
+  reducer:{}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
   </React.StrictMode>
 );
 
